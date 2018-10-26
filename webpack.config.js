25bebf9b506533e5250735692eaea58fd766d0cb
Kyle Combs <combskyle@gmail.com>
2018-10-25 20:24:41 -0400

    initialized our webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+
+const outputDir = 'dist';
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.join(__dirname, outputDir),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|ttf|svg|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 10000000 }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  plugins: [
+    new CleanWebPackPlugin([outputDir]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.png'
+    })
+  ]
+};
