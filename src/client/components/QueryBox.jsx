d7e43f9513a1212308d71e0f1140b16d16f772f3
Jason Ou <jasonou@Jasons-MacBook-Pro.local>
2018-10-27 15:30:11 -0400

    query formatting

--- src/client/components/QueryBox.jsx
+++ src/client/components/QueryBox.jsx
@@ -8,13 +8,21 @@ class QueryBox extends React.Component {
 
   }
 
+  formatQueryString(string) {
+    string = string.replace(/select/gi, 'SELECT');
+    string = string.replace(/from/gi, 'FROM');
+    string = string.replace(/where/gi, 'WHERE');
+    string = string.replace(/on/gi, 'ON');
+    return string;
+  }
+
   render(){
     return (      
       <div>
         <div className='queryBox'>
           <form onSubmit={this.props.handleQuerySubmit}>
             <div className='test'></div>
-            <textarea value={this.props.queryString} onChange={this.props.handleQueryChange} />
+            <textarea value={this.formatQueryString(this.props.queryString)} onChange={this.props.handleQueryChange} />
             <div className='queryButton'><button id='submit-button' value="Submit">Query</button></div>
           </form>  
         </div>
